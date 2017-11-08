204f5c7ece1cd8b5896cca52289e03161e41a611
chenbin <chenbin@my089.net>
2017-11-08 09:22:42 +0800

    1.spring 定时任务2。

--- src/main/java/hello/ScheduledTasks.java
+++ src/main/java/hello/ScheduledTasks.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -19,8 +20,13 @@ public class ScheduledTasks {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)//每5秒执行一次
-    public void reportCurrentTime(){
-        log.info("The time is new {}",dateFormat.format(new Date()));
+//    @Scheduled(fixedRate = 5000)//每5秒执行一次
+//    public void reportCurrentTime(){
+//        log.info("The time is new {}",dateFormat.format(new Date()));
+//    }
+
+    @Scheduled(cron="0 30/5 * * * ?") //每小时的33分钟开始执行，每3分钟执行1次
+    public void start() throws ServletException {
+        log.info(String.format("每小时的30分钟开始执行，每5分钟执行1次,%s",dateFormat.format(new Date())));
     }
 }
